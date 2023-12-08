d0906e0ed7d69e9b9b22854a3f4707969f613527
Deadsocks <deadsocks@github.com>
2023-12-08 11:42:38 +0100

    day 5

--- day_05.py
+++ day_05.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import copy
 
 def split_into_eight_lists(input_string):
     string_lists = input_string.split('\n\n')
@@ -57,6 +58,32 @@ def get_location_of_seed(seed, map_lists):
 
     return source[0]
 
+
+def process_ranges(A, B):
+    # Ensure the ranges are sorted (lower bound first)
+    A.sort()
+    B.sort()
+
+    # Initialize the lists for intersection and differences
+    intersection = []
+    lower_diff = []
+    upper_diff = []
+
+    # Calculate intersection and differences
+    if B[0] < A[0]:
+        lower_diff = [B[0], min(A[0], B[1])]
+
+    if B[1] > A[1]:
+        upper_diff = [max(A[1], B[0]), B[1]]
+
+    intersection_start = max(A[0], B[0])
+    intersection_end = min(A[1], B[1])
+
+    if intersection_start < intersection_end:
+        intersection = [intersection_start, intersection_end]
+
+    return intersection, lower_diff, upper_diff
+
 def map_range_in_location_space_to_seed_range(map_lists):
 
 
@@ -64,37 +91,89 @@ def map_range_in_location_space_to_seed_range(map_lists):
     list_of_adds = []
 
     for i in range(len(map_lists)):
+        list_of_ranges.append([])
+        list_of_adds.append([])
         current_list = map_lists[i]
         for j in range(len(current_list)):
             current_line = current_list[j]
 
-            range_limits = [current_line[0], current_line[0]+current_line[2]-1]
+            range_limits = [current_line[0], current_line[0]+current_line[2]]
             add = current_line[1]-current_line[0]
-            list_of_ranges.append(range_limits)
-            list_of_adds.append(add)
+            list_of_ranges[i].append(range_limits)
+            list_of_adds[i].append(add)
 
 
-    new_ranges = divide_ranges(list_of_ranges)
+    temp_list_of_ranges = []
+    for list in list_of_ranges:
+        for entry in list:
+            temp_list_of_ranges.append(entry)
+    new_ranges = divide_ranges(temp_list_of_ranges)
 
     new_ranges = np.array(new_ranges)
 
-    new_ranges[:, 1] -= 1
+    #new_ranges[:, 1] -= 1
 
-    new_list_of_adds = []
+    new_list_of_adds = [0 for _ in range(len(new_ranges))]
 
-    for i in range(len(new_ranges)):
-        current_range = new_ranges[i]
-        sum = 0
-        for j in range(len(list_of_ranges)):
-            old_range = list_of_ranges[j]
+    for i in range(len(list_of_ranges)):
+        prev_list_of_adds = copy.deepcopy(new_list_of_adds)
+        test_2 = list_of_ranges[i]
+        for k in range(len(list_of_ranges[i])):
+            old_range = list_of_ranges[i][k]
+            sum = 0
+            j = 0
+            while j < len(new_ranges):
+                current_range = new_ranges[j]
+                current_range_temp = current_range+prev_list_of_adds[j]
+                """if old_range[0] <= current_range_temp[0]:
+                    if current_range_temp[1] <= old_range[1]:"""
 
-            if old_range[0] <= current_range[0]:
-                if current_range[1] <= old_range[1]:
-                    sum += list_of_adds[j]
+                # Check for the different types of overlap:
+                # current_range_temp = [crt[0], crt[1]], old_range = [or[0], [or[1]]
+                # or[0] <= crt[0] and crt[1] <= or[1] -> all good
+                # or[0] > crt[0] and crt[1] <= or[1] -> create new lists [crt[0], or[0]] and [or[0], crt[1]]
 
-        new_list_of_adds.append(sum)
+
+                if check_overlap(old_range, current_range_temp):
+
+                    intersection, lower_diff, upper_diff = process_ranges(old_range, current_range_temp)
+                    not_complete_intersection = False
+                    if len(lower_diff) != 0:
+                        lower_diff = lower_diff-new_list_of_adds[j]
+                        not_complete_intersection = True
+                        new_ranges = np.append(new_ranges, np.array(lower_diff)[np.newaxis, :], axis=0)
+                        new_list_of_adds.append(new_list_of_adds[j])
+                        prev_list_of_adds.append(new_list_of_adds[j])
+
+                    if len(upper_diff) != 0:
+                        upper_diff = upper_diff-new_list_of_adds[j]
+                        not_complete_intersection = True
+                        new_ranges = np.append(new_ranges, np.array(upper_diff)[np.newaxis, :], axis=0)
+                        new_list_of_adds.append(new_list_of_adds[j])
+                        prev_list_of_adds.append(new_list_of_adds[j])
+
+                    if not_complete_intersection:
+                        intersection = intersection-new_list_of_adds[j]
+                        new_ranges[j] = np.array(intersection)
+
+                    #if old_range[1] != current_range[0]:
+                    new_list_of_adds[j] += list_of_adds[i][k]
+                    test = 1
+
+
+                j += 1
+
+            #print("test")
+
+    # Sort arrays:
+    arr1inds = new_ranges[:, 0].argsort()
+
+    new_ranges = new_ranges[arr1inds]
+    new_list_of_adds = np.array(new_list_of_adds)
+    new_list_of_adds[arr1inds]
 
     return new_ranges, new_list_of_adds
+
 def divide_ranges(ranges):
     # Flatten the list and sort the endpoints
     endpoints = sorted(set([point for range_ in ranges for point in range_]))
@@ -109,7 +188,7 @@ def divide_ranges(ranges):
     return new_ranges
 
 def check_overlap(A, B):
-    return A[0] <= B[1] and B[0] <= A[1]
+    return A[0] < B[1] and B[0] < A[1]
 
 
 def do_task_1(file_name):
@@ -150,6 +229,7 @@ def do_task_2(file_name):
 
     for i in range(len(location_range_to_seed_range)):
         test = location_range_to_seed_range[i]
+        test_1 = location_to_seed_add[i]
         location_to_seed_range = location_range_to_seed_range[i] + location_to_seed_add[i]
         for j in range(len(new_seed_list)):
             seed_range = new_seed_list[j]
